ea8e63548809d649231a5c3acf3007d7aab86e85
hpramosdev <henrique.pramos@gmail.com>
2024-06-17 14:23:53 -0300

    correção de exercício

--- cpp/vehicle-purchase/vehicle_purchase.cpp
+++ cpp/vehicle-purchase/vehicle_purchase.cpp
@@ -5,14 +5,15 @@
 namespace vehicle_purchase {
 
     // needs_license determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
-    bool needs_license(std::string kind){
+    bool needs_license(std::string kind) {
         // TODO: Return true if you need a license for that kind of vehicle.
         if (kind == "car" || kind == "truck") {
             return true;
-        } else {
+        }
+        else {
             return false;
         }
-        
+
     }
 
     // choose_vehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
@@ -20,17 +21,13 @@ namespace vehicle_purchase {
         // TODO: Return the final decision in a sentence.
         int result = option1.compare(option2);
         std::string message;
-        
-        switch (result) {            
-            case -1:
-                message = option1 + " is clearly the better choice.";
-            break;
-
-            case 1:
-                message = option2 + " is clearly the better choice.";
-            break;
-
+        if (result < 0) {
+            message = option1 + " is clearly the better choice.";
         }
+        else if (result > 0) {
+            message = option2 + " is clearly the better choice.";
+        }
+
         return message;
     }
 
@@ -40,11 +37,13 @@ namespace vehicle_purchase {
         double resell_price;
 
         if (age < 3) {
-            resell_price = original_price * (1 - 0.8 / 100);
-        } else if (age >= 10) {
-            resell_price = original_price * (1 - 0.5 / 100);
-        } else {
-            resell_price = original_price * (1 - 0.7 / 100);
+            resell_price = original_price * (1 - 20.0 / 100.0);
+        }
+        else if (age >= 10) {
+            resell_price = original_price * (1 - 50.0 / 100.0);
+        }
+        else {
+            resell_price = original_price * (1 - 30.0 / 100.0);
         }
 
         return  resell_price;
